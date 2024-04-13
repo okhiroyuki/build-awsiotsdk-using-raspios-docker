21cdb558afa9f06ad497a31136de8bfbd4b62389
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-14 00:12:40 +0900

    chore: test

--- import_docker_image.sh
+++ import_docker_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+echo "Downloading the latest Raspbian Lite image..."
 wget https://downloads.raspberrypi.org/raspios_lite_armhf/root.tar.xz -q
 
+echo "Importing the Raspbian Lite image into Docker..."
 docker image import ./root.tar.xz raspios-lite-armhf:2022-04-04
-
-docker images
