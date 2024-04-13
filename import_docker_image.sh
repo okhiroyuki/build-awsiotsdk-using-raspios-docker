b16981ad59d0c808fb09be764ae4ab29846c8c65
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-14 00:05:11 +0900

    fix: docker name

--- import_docker_image.sh
+++ import_docker_image.sh
@@ -2,6 +2,6 @@
 
 wget https://downloads.raspberrypi.org/raspios_lite_armhf/root.tar.xz -q
 
-docker image import ./root.tar.xz raspios_lite_armhf:2022-04-04
+docker image import ./root.tar.xz raspios-lite-armhf:2022-04-04
 
 docker images
