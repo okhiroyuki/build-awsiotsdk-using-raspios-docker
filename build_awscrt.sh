924960b0fb4c5084c977e2084f490ab6b2ff5787
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-14 10:14:59 +0900

    feat: add awscrt ci

---
+++ build_awscrt.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -xeu
+
+# Display the OS information
+cat /etc/os-release
+cat /etc/issue
+cat /proc/version
+lsb_release -a
+lscpu | grep -e "Architecture" -e "Model name"
+uname -a
+
+# Install dependencies
+apt-get update -y \
+&& apt-get install git python3-pip -y --no-install-recommends
+
+# Clone the AWS Common Runtime (CRT) for Python repository
+git clone https://github.com/awslabs/aws-crt-python.git
+git checkout v0.20.5
+cd aws-crt-python
+git submodule update --init
+python3 -m pip install .
+
+# Install the dependencies and build the wheel package
+python3 setup.py bdist_wheel
