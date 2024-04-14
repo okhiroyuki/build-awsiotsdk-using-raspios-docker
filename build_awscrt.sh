371a5ebad310822530e831030aa299bfbf795dd3
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-14 12:36:11 +0900

    chore: test

--- build_awscrt.sh
+++ build_awscrt.sh
@@ -19,6 +19,12 @@ apt-get update -y \
 # Install pyenv
 curl https://pyenv.run | bash
 
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+exec "$SHELL"
+
 pyenv install
 
 # Set the default directory for the safe command
