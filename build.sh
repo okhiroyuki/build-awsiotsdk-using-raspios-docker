0b7505310e2fefda13a7581c9bc2f264d18f3678
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-14 09:55:06 +0900

    feat: add build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -xeu
+
+# Display the OS information
+cat /etc/os-release
+cat /etc/issue
+cat /proc/version
+lsb_release -a
+lscpu | grep -e "Architecture" -e "Model name"
+uname -a
+
+# Install dependencies
+apt-get update -y \
+&& apt-get install git python3-pip -y --no-install-recommends
+
+# Clone the AWS IoT Device SDK for Python v2 repository
+git clone https://github.com/aws/aws-iot-device-sdk-python-v2.git \
+
+# Install the dependencies and build the wheel package
+cp requirements.txt aws-iot-device-sdk-python-v2/
+cd aws-iot-device-sdk-python-v2
+python3 -m pip install -r requirements.txt
+python3 setup.py bdist_wheel
