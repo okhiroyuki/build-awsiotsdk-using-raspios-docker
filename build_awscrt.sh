c0253f0d14d3d69886f64f77defa2b0d647d0b6a
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-14 12:24:48 +0900

    chore: test

--- build_awscrt.sh
+++ build_awscrt.sh
@@ -10,7 +10,7 @@ cat /etc/issue
 cat /proc/version
 lsb_release -a
 lscpu | grep -e "Architecture" -e "Model name"
-uname -a
+uname -m
 
 # Install dependencies
 apt-get update -y \
@@ -19,11 +19,13 @@ apt-get update -y \
 # Set the default directory for the safe command
 git config --global --add safe.directory /app
 
-# Clone the AWS Common Runtime (CRT) for Python repository
-git clone https://github.com/awslabs/aws-crt-python.git -b $TAG
-cd aws-crt-python
-git submodule update --init
-python3 -m pip install .
+python3 -V
 
-# Install the dependencies and build the wheel package
-python3 setup.py bdist_wheel
+# # Clone the AWS Common Runtime (CRT) for Python repository
+# git clone https://github.com/awslabs/aws-crt-python.git -b $TAG
+# cd aws-crt-python
+# git submodule update --init
+# python3 -m pip install .
+
+# # Install the dependencies and build the wheel package
+# python3 setup.py bdist_wheel
