ebea8ff3c5467591da45c6a4ede0a4c394245fa3
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-14 12:38:44 +0900

    chore: test

--- build_awscrt.sh
+++ build_awscrt.sh
@@ -23,7 +23,7 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
-exec "$SHELL"
+source ~/.bashrc
 
 pyenv install
 
